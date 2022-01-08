daac20b5446714278bd7d57151b92378431d3912
lucaslomeu <lucaslomeugomes@gmail.com>
2022-01-08 17:23:29 -0300

    Update Input

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Header.scss';
 
+import { FiLogOut, FiSettings } from 'react-icons/fi';
+
 const Header = () => {
   return (
     <div className="header">
-      <div className="logo-header">in.Pulse CRM</div>
-      <div className="info-operator">Lucas Lomeu</div>
+      <div className="logo">InPulse</div>
+      <div className="info-operator">
+        <div className="name-operator info-item">Lucas Lomeu</div>
+        <FiSettings className="info-item" />
+        <FiLogOut className="info-item" />
+      </div>
     </div>
   );
 };
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Input.scss';
 
-const Input = ({ titleInput, type, placeholder, disabled = 'false' }) => {
+const Input = ({ titleInput, type, placeholder }) => {
   return (
     <div className="inputForm">
       <h1>{titleInput}</h1>
-      <input type={type} placeholder={placeholder} disabled={disabled} />
+      <input type={type} placeholder={placeholder} />
     </div>
   );
 };
